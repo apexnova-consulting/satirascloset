226c04cd6afa461f2aef69c14c712a2721e9b96c
Your Name <you@example.com>
2025-04-10 22:51:03 -0400

    Fix: Add 'use client' directive to components using React hooks

--- satiras-closet/components/Header.tsx
+++ satiras-closet/components/Header.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import { useState } from 'react';
 import Link from 'next/link';
 import { ShoppingBagIcon, UserIcon, Bars3Icon, XMarkIcon, MagnifyingGlassIcon } from '@heroicons/react/24/outline';
